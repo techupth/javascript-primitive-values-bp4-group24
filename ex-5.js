f9394e01a87cc51235bbd1c6c2191ddc8816e705
zvryn <sarinkanok@gmail.com>
2024-05-28 15:11:59 +0700

    feat: add completed assignment primitive value

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
-//Exercise #5: Shopping and Discount
-
+// Exercise #5: Shopping and Discount
 // Start coding here
+let totalPrice = 27000;
+let discount25percent = totalPrice*0.25;
+let afterDiscountPrice = totalPrice-discount25percent;
+
+console.log(afterDiscountPrice);
